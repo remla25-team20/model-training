2082172ba676abb30e5890c6bd88aac4851aa26b
Nathan Blum <n.blum@student.tudelft.nl>
2025-05-23 18:26:57 +0200

    Added metrics, REFERENCE STATE

--- src/modeling/evaluation.py
+++ src/modeling/evaluation.py
@@ -1,12 +1,13 @@
 import joblib
 import dvc.api
+import json
 
 from pathlib import Path
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 PARAMS = dvc.api.params_show("params.yaml")
 
-def predict(model_dir: Path, interim_data_dir: Path):
+def predict(model_dir: Path, interim_data_dir: Path, metrics_directory: Path):
     classifier = joblib.load(model_dir / "Sentiment_Analysis_Model.joblib")
     X_test = joblib.load(interim_data_dir / "X_test.joblib")
     y_test = joblib.load(interim_data_dir / "y_test.joblib")
@@ -17,9 +18,12 @@ def predict(model_dir: Path, interim_data_dir: Path):
     print(cm)
     accuracy = accuracy_score(y_test, y_pred)
     print(f"Accuracy: {accuracy * 100:.2f}%")
+    with open(metrics_directory / "accuracy.json", "w") as out:
+        json.dump({"accuracy": accuracy}, out)
 
 if __name__ == "__main__":
     predict(
         model_dir=Path(PARAMS['models_directory']),
-        interim_data_dir=Path(PARAMS['interim_data_directory'])
+        interim_data_dir=Path(PARAMS['interim_data_directory']),
+        metrics_directory=Path(PARAMS['metrics_directory'])
         )
